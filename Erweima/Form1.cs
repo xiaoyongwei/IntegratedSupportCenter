2e1e0ba2a7a7b3106cb4b53b187ee5309b9f420a
xiaoyongwei <xywabc@163.com>
2020-04-07 22:30:45 +0800

    2020年4月7日22时30分39秒

--- Erweima/Form1.cs
+++ Erweima/Form1.cs
@@ -3,12 +3,15 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
 using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using ZXing;
+using ThoughtWorks.QRCode.Codec;
+
 
 namespace Erweima
 {
@@ -21,50 +24,74 @@ namespace Erweima
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            OpenFileDialog ofd = new OpenFileDialog();   //创建一个对话框对象
-            ofd.Title = "请选择上传的图片";  //为对话框设置标题
-            ofd.Filter = "图片格式|*.bmp";  //设置筛选的图片格式
-            ofd.Multiselect = false;        //设置是否允许多选
+            string nrText = DateTime.Now.Millisecond + "_成品_彩盒_C200401018_2h-2_998";
+            HebingPicture(CreateCode_Simple(nrText,nrText),
+                "background1.jpg",nrText);
+        }
+        //生成二维码方法一
+        private static string CreateCode_Simple(string nr,string filename)
+        {
+            QRCodeEncoder qrCodeEncoder = new QRCodeEncoder();
+            qrCodeEncoder.QRCodeEncodeMode = QRCodeEncoder.ENCODE_MODE.BYTE;
+            qrCodeEncoder.QRCodeScale = 4;
+            qrCodeEncoder.QRCodeVersion = 8;
+            qrCodeEncoder.QRCodeErrorCorrect = QRCodeEncoder.ERROR_CORRECTION.M;
+            System.Drawing.Image image = qrCodeEncoder.Encode(nr);
+            string FileFullFileName = "picture\\" + filename+".jpg";
+            System.IO.FileStream fs = new System.IO.FileStream(FileFullFileName, System.IO.FileMode.OpenOrCreate, System.IO.FileAccess.Write);
+            image.Save(fs, System.Drawing.Imaging.ImageFormat.Jpeg);
 
-            if (ofd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
-            {
-                string filePath = ofd.FileName;//获得文件的完整路径（包括名字后后缀）
-                Text = filePath;//将文件路径显示在文本框中
-                int position = filePath.LastIndexOf("\\");
-                string fileName = filePath.Substring(position + 1);
-                using (Stream stream = ofd.OpenFile())
-                {
-                    using (FileStream fs = new FileStream(fileName, FileMode.Create))
-                    {
-                        stream.CopyTo(fs);
-                        fs.Flush();
-                    }
-                    this.pictureBox1.ImageLocation = fileName;
-                }
-            }
+            fs.Close();
+            image.Dispose();
+            return FileFullFileName;
         }
 
         /// <summary>
-        /// 生成二维码
+        /// 合并图片
         /// </summary>
-        /// <param name="msg">二维码信息</param>
-        /// <returns>图片</returns>
-        private Bitmap GenByZXingNet(string msg)
+        private void HebingPicture(string erweimaFile,string backgroundFile,string text)
         {
-            BarcodeWriter writer = new BarcodeWriter();
-            writer.Format = BarcodeFormat.QR_CODE;
-            writer.Options.Hints.Add(EncodeHintType.CHARACTER_SET, "UTF-8");//编码问题
-            writer.Options.Hints.Add(
-                EncodeHintType.ERROR_CORRECTION,
-                ZXing.QrCode.Internal.ErrorCorrectionLevel.H
-
-            );
-            const int codeSizeInPixels = 250;   //设置图片长宽
-            writer.Options.Height = writer.Options.Width = codeSizeInPixels;
-            writer.Options.Margin = 0;//设置边框
-            ZXing.Common.BitMatrix bm = writer.Encode(msg);
-            Bitmap img = writer.Write(bm);
-            return img;
+            pictureBox1.Image = Image.FromFile(erweimaFile); //设置背景图片
+            string imgPath = backgroundFile;  //要插入的二维码图片路径
+            Image QRcodePic; //用来存储读取的二维码图片
+            //读取二维码图片文件流
+            FileStream fileStream = new FileStream(imgPath, FileMode.Open, FileAccess.Read);
+            int byteLength = (int)fileStream.Length;    //二维码图片字节数
+            byte[] fileBytes = new byte[byteLength];    //根据图片字节数创建一个存储该图片的字节数组
+            fileStream.Read(fileBytes, 0, byteLength);  //读取二维码图片到数组
+            fileStream.Close();                 //关闭文件流，解除对外部文件的锁定
+            //取得二维码图片image对象
+            QRcodePic = Image.FromStream(new MemoryStream(fileBytes));
+            Graphics g = Graphics.FromImage(pictureBox1.Image);  //创建背景图片的Graphics对象（调用该对象在背景图片上绘图）
+            //在背景图片上插入二维码图片
+            g.DrawImage(QRcodePic, 500, 120, QRcodePic.Width, QRcodePic.Height);
+            //在背景照片上添加文字 
+            PointF drawPoint = new PointF(55.0F, 160.0F);//
+            AddFont(g, drawPoint, text);
+            //drawPoint = new PointF(55.0F, 220.0F);//
+            //AddFont(g, drawPoint, "MoviePark:IT爱好者");
+            //drawPoint = new PointF(55.0F, 280.0F);//
+            //AddFont(g, drawPoint, "Blog:blog.csdn.net/u012577474");
+            //刷新pictureBox1
+            pictureBox1.Refresh();
         }
+
+
+        /*在图片上添加文字
+         * Graphics g  ，目标Graphics对象     
+         * string data  ，准备添加的字符串
+         */
+        private void AddFont(Graphics g, PointF drawPoint, string data)
+        {
+            //   Graphics g = Graphics.FromImage(pictureBox1.Image);
+            SolidBrush mybrush;
+            mybrush = new SolidBrush(Color.Red);  //设置默认画刷颜色
+            Font myfont;
+            myfont = new Font("黑体", 10, FontStyle.Bold);         //设置默认字体格式   
+            g.DrawString(data, myfont, mybrush, drawPoint); //图片上添加文字
+            pictureBox1.Refresh();
+
+        }
+
     }
 }
