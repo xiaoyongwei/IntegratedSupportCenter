cb1c959e637f13f4f9bef0a4258f3e85dc7def7e
xiaoyongwei <40009957+xiaoyongwei@users.noreply.github.com>
2021-05-31 21:48:19 +0800

    2021年5月31日21时48分15秒

---
+++ 工作数据分析/WinForm/WuLiu/yunfei/Form运费编辑.Designer.cs
@@ -0,0 +1,116 @@
+﻿
+namespace 工作数据分析.WinForm.WuLiu.yunfei
+{
+    partial class Form运费编辑
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgv = new System.Windows.Forms.DataGridView();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.保存ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.还原ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.关闭ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dgv
+            // 
+            this.dgv.AllowUserToAddRows = false;
+            this.dgv.AllowUserToDeleteRows = false;
+            this.dgv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgv.Location = new System.Drawing.Point(0, 28);
+            this.dgv.Name = "dgv";
+            this.dgv.RowHeadersWidth = 51;
+            this.dgv.RowTemplate.Height = 27;
+            this.dgv.Size = new System.Drawing.Size(800, 422);
+            this.dgv.TabIndex = 0;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.保存ToolStripMenuItem,
+            this.还原ToolStripMenuItem,
+            this.关闭ToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 28);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // 保存ToolStripMenuItem
+            // 
+            this.保存ToolStripMenuItem.Name = "保存ToolStripMenuItem";
+            this.保存ToolStripMenuItem.Size = new System.Drawing.Size(53, 24);
+            this.保存ToolStripMenuItem.Text = "保存";
+            this.保存ToolStripMenuItem.Click += new System.EventHandler(this.保存ToolStripMenuItem_Click);
+            // 
+            // 还原ToolStripMenuItem
+            // 
+            this.还原ToolStripMenuItem.Name = "还原ToolStripMenuItem";
+            this.还原ToolStripMenuItem.Size = new System.Drawing.Size(53, 24);
+            this.还原ToolStripMenuItem.Text = "还原";
+            // 
+            // 关闭ToolStripMenuItem
+            // 
+            this.关闭ToolStripMenuItem.Name = "关闭ToolStripMenuItem";
+            this.关闭ToolStripMenuItem.Size = new System.Drawing.Size(53, 24);
+            this.关闭ToolStripMenuItem.Text = "关闭";
+            // 
+            // Form运费编辑
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dgv);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.MinimizeBox = false;
+            this.Name = "Form运费编辑";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "运费编辑";
+            this.Load += new System.EventHandler(this.Form运费编辑_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgv;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 保存ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 还原ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 关闭ToolStripMenuItem;
+    }
+}
---
+++ 工作数据分析/WinForm/WuLiu/yunfei/Form运费编辑.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using 工作数据分析.Data.DAL.Oracle;
+using 工作数据分析.Properties;
+using 综合保障中心.Comm;
+
+namespace 工作数据分析.WinForm.WuLiu.yunfei
+{
+    public partial class Form运费编辑 : Form
+    {
+        private string IDS = "";
+        public Form运费编辑(string ids)
+        {
+            this.IDS = ids;
+            InitializeComponent();
+        }
+
+        private void Form运费编辑_Load(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(IDS))
+            {
+                My.ShowErrorMessage("数据为空!");
+                return;
+            }
+            if (dgv.Rows.Count>10)
+            {
+                My.ShowErrorMessage("数据太多了,不能超过10条!");
+                return;
+            }
+            this.dgv.DataSource = OracleHelper.ExecuteDataTable(
+                Resources.运费结算
+                +" and id in("+IDS+")");
+            dgv.AutoResizeColumns();
+            this.WindowState = FormWindowState.Maximized;
+        }
+
+        private void 保存ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string updateString =
+            "UPDATE FERP.DLV_FARE SET ACCAMT ={0},ANNAMT ={1},ANNAMT2 ={2}," +
+            "ANNAMT3 ={3},USMARK ='{4}'	WHERE ID={5} ";
+            List<string> sqlList = new List<string>();
+            foreach (DataGridViewRow row in dgv.Rows)
+            {
+                sqlList.Add(
+                   string.Format(updateString
+                  , GetCellValueYunfei(row.Cells["运费"])
+                  , GetCellValueYunfei(row.Cells["ANNAMT"])
+                  , GetCellValueYunfei(row.Cells["补运费"])
+                   , GetCellValueYunfei(row.Cells["ANNAMT3"])
+                   , GetCellValueYunfei(row.Cells["备注"])
+                   , row.Cells["ID"].Value.ToString()
+                   
+                    ));
+            }
+            if(OracleHelper.ExecuteSqlTran(sqlList))
+            {
+                My.ShowMessage("保存成功!");
+                this.DialogResult = DialogResult.OK;
+            }
+            else
+            {
+                My.ShowErrorMessage("保存失败!!");
+                this.DialogResult = DialogResult.None;
+            }
+        }
+
+        private string GetCellValueYunfei(DataGridViewCell cell)
+        {
+            return cell.Value == null
+                   || string.IsNullOrWhiteSpace(cell.Value.ToString())
+                   ? "null" : cell.Value.ToString();
+        }
+    }
+}
